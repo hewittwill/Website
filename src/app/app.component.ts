4769e83ec8997bee783900af7c1fcf4fc12ad3b7
Will Hewitt <me@hewittwill.com>
2020-05-18 14:00:51 +1200

    fixed 404

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Website';
+  title = 'Will Hewitt';
+
+  constructor(private router: Router) {
+    let path = localStorage.getItem('path');
+    if(path) {
+      localStorage.removeItem('path');
+      this.router.navigate([path]);
+    }
+  }
+
 }
